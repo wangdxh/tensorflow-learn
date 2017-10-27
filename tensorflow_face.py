3f409630a41aec8225f2faad4ae7c422fcfa42ba
wangdxh <wangdxh@163.com>
2017-10-27 15:40:14 +0800

    new file
    new file

--- tensorflow_face.py
+++ tensorflow_face.py
@@ -20,6 +20,7 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import cv2
+import tensorflow_face_conv as myconv
 
 def createdir(*args):
     ''' create dir'''
@@ -27,10 +28,14 @@ def createdir(*args):
         if not os.path.exists(item):
             os.makedirs(item)
 
-DIR_MYFACE = './image/my_faces'
-DIR_PEOPLEFACE = './image/people_faces'
+DIR_MY_FACE = './image/my_faces'
+DIR_PEOPLE_FACE = './image/people_faces'
 
-createdir(DIR_MYFACE, DIR_PEOPLEFACE)
+DIR_TEST_MY_FACE = './image/test_my_faces'
+DIR_TEST_PEOPLE_FACE = './image/test_people_faces'
+IMGSIZE = 64
+
+createdir(DIR_MY_FACE, DIR_PEOPLE_FACE, DIR_TEST_PEOPLE_FACE)
 
 def getpaddingSize(shape):
     ''' get size to make image to be a square rect '''
@@ -39,9 +44,9 @@ def getpaddingSize(shape):
     result = (np.array([longest]*4, int) - np.array([h, h, w, w], int)) // 2
     return result.tolist()
 
-def dealwithimage(imgpath, h=64, w=64):
+def dealwithimage(img, h=64, w=64):
     ''' dealwithimage '''
-    img = cv2.imread(imgpath)
+    #img = cv2.imread(imgpath)
     top, bottom, left, right = getpaddingSize(img.shape[0:2])
     img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[0, 0, 0])
     img = cv2.resize(img, (h, w))
@@ -68,7 +73,8 @@ def getface(imgpath, outdir):
         n += 1
         face = img[f_y:f_y+f_h, f_x:f_x+f_w]
         # may be do not need resize now
-        face = cv2.resize(face, (64, 64))
+        #face = cv2.resize(face, (64, 64))
+        face = dealwithimage(face, IMGSIZE, IMGSIZE)
         for inx, (alpha, bias) in enumerate([[1, 1], [1, 50], [0.5, 0]]):
             facetemp = relight(face, alpha, bias)
             cv2.imwrite(os.path.join(outdir, '%s_%d_%d.jpg' % (filename, n, inx)), facetemp)
@@ -88,6 +94,18 @@ def generateface(pairdirs):
         for fileitem in getfilesinpath(inputdir):
             getface(fileitem, outputdir)
 
+def readimage(pairpathlabel):
+    '''read image to list'''
+    imgs = []
+    labels = []
+    for filepath, label in pairpathlabel:
+        for fileitem in getfilesinpath(filepath):
+            img = cv2.imread(fileitem)
+            imgs.append(img)
+            labels.append(label)
+    return np.array(imgs), np.array(labels)
+
+
 def main(_):
     ''' main '''
     savepath = './checkpoint/face.ckpt'
@@ -97,15 +115,26 @@ def main(_):
     if isneedtrain:
         #first generate all face
         log.debug('generateface')
-        generateface([['./image/people_images', './image/people_faces'],
-                      ['./image/my_images', './image/my_faces']])
+        generateface([['./image/people_images', DIR_PEOPLE_FACE],
+                      ['./image/my_images', DIR_MY_FACE]])
         # then read file and tarin
 
-    #restore session and judge file
+        train_x, train_y = readimage([[DIR_MY_FACE, [1, 0]], [DIR_PEOPLE_FACE, [0, 1]]])
+        train_x = train_x.astype(np.float32) / 255.0
+        log.debug('len of train_x : %s', train_x.shape)
+        myconv.train(train_x, train_y, savepath)
+        log.debug('training is over, please run again')
+    else:
+        generateface([['./image/test_people_images', DIR_TEST_PEOPLE_FACE]])
+        test_x, test_y = readimage([[DIR_TEST_PEOPLE_FACE, [0, 1]]])
+        test_x = test_x.astype(np.float32) / 255.0
+        log.debug('len of test_x : %s', test_x.shape)
+        log.debug('y is %s', test_y)
+        log.debug(myconv.validate(test_x, test_y, savepath))
 
 if __name__ == '__main__':
     # first generate all face
     main(0)
-    
+
 
 
---
+++ tensorflow_face_conv.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+#coding=utf-8
+''' face detect convolution'''
+# pylint: disable=invalid-name
+import os
+import logging as log
+import matplotlib.pyplot as plt
+import common
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import cv2
+
+SIZE = 64
+x_data = tf.placeholder(tf.float32, [None, SIZE, SIZE, 3])
+y_data = tf.placeholder(tf.float32, [None, 2])
+
+keep_prob_5 = tf.placeholder(tf.float32)
+keep_prob_75 = tf.placeholder(tf.float32)
+
+def weightVariable(shape):
+    ''' build weight variable'''
+    init = tf.random_normal(shape, stddev=0.01)
+    return tf.Variable(init)
+
+def biasVariable(shape):
+    ''' build bias variable'''
+    init = tf.random_normal(shape)
+    return tf.Variable(init)
+
+def conv2d(x, W):
+    ''' conv2d by 1, 1, 1, 1'''
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def maxPool(x):
+    ''' max pooling'''
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+def dropout(x, keep):
+    ''' drop out'''
+    return tf.nn.dropout(x, keep)
+
+def cnnLayer():
+    ''' create cnn layer'''
+    # 第一层
+    W1 = weightVariable([3, 3, 3, 32]) # 卷积核大小(3,3)， 输入通道(3)， 输出通道(32)
+    b1 = biasVariable([32])
+    conv1 = tf.nn.relu(conv2d(x_data, W1) + b1)
+    pool1 = maxPool(conv1)
+    # 减少过拟合，随机让某些权重不更新
+    drop1 = dropout(pool1, keep_prob_5) # 32 * 32 * 32 多个输入channel 被filter内积掉了
+
+    # 第二层
+    W2 = weightVariable([3, 3, 32, 64])
+    b2 = biasVariable([64])
+    conv2 = tf.nn.relu(conv2d(drop1, W2) + b2)
+    pool2 = maxPool(conv2)
+    drop2 = dropout(pool2, keep_prob_5) # 64 * 16 * 16
+
+    # 第三层
+    W3 = weightVariable([3, 3, 64, 64])
+    b3 = biasVariable([64])
+    conv3 = tf.nn.relu(conv2d(drop2, W3) + b3)
+    pool3 = maxPool(conv3)
+    drop3 = dropout(pool3, keep_prob_5) # 64 * 8 * 8
+
+    # 全连接层
+    Wf = weightVariable([8*16*32, 512])
+    bf = biasVariable([512])
+    drop3_flat = tf.reshape(drop3, [-1, 8*16*32])
+    dense = tf.nn.relu(tf.matmul(drop3_flat, Wf) + bf)
+    dropf = dropout(dense, keep_prob_75)
+
+    # 输出层
+    Wout = weightVariable([512, 2])
+    bout = weightVariable([2])
+    #out = tf.matmul(dropf, Wout) + bout
+    out = tf.add(tf.matmul(dropf, Wout), bout)
+    return out
+
+def train(train_x, train_y, tfsavepath):
+    ''' train'''
+    log.debug('train')
+    out = cnnLayer()
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out, labels=y_data))
+    train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), tf.argmax(y_data, 1)), tf.float32))
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        batch_size = 10
+        num_batch = len(train_x) // 10
+        for n in range(10):
+            for i in range(num_batch):
+                batch_x = train_x[i*batch_size : (i+1)*batch_size]
+                batch_y = train_y[i*batch_size : (i+1)*batch_size]
+                _, loss = sess.run([train_step, cross_entropy],\
+                                   feed_dict={x_data:batch_x, y_data:batch_y,
+                                              keep_prob_5:0.5, keep_prob_75:0.75})
+
+                print(n*num_batch+i, loss)
+
+        # 获取测试数据的准确率
+        acc = accuracy.eval({x_data:train_x, y_data:train_y, keep_prob_5:1.0, keep_prob_75:1.0})
+        print('after 10 times run: accuracy is ', acc)
+        saver.save(sess, tfsavepath)
+
+def validate(test_x, test_y, tfsavepath):
+    ''' validate '''
+    output = cnnLayer()
+    predict = tf.equal(tf.argmax(output, 1), tf.argmax(y_data, 1))
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, tfsavepath)
+        res = sess.run(predict, feed_dict={x_data: test_x, y_data: test_y,
+                                           keep_prob_5:1.0, keep_prob_75: 1.0})
+        return res
+
+if __name__ == '__main__':
+    pass
